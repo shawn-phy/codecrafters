73f261dc05a632c9179025d11dcbedc745c7e41a
shawn_phy <61213955+shawn-phy@users.noreply.github.com>
2023-07-25 02:17:48 +0300

    pass for the second stage

--- main.c
+++ main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	// Disable output buffering
 	setbuf(stdout, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
-
+	int StdOutErrToParent[2];
+	pipe(StdOutErrToParent);
 	
+
 	char *command = argv[3];
 	int child_pid = fork();
-	if (child_pid == -1) {
-	    printf("Error forking!");
-	    return 1;
+	if (child_pid == -1)
+	{
+		printf("Error forking!");
+		return 1;
 	}
-	
-	if (child_pid == 0) {
-		   // Replace current program with calling program
-	    execv(command, &argv[3]);
-	} else {
-		   // We're in parent
-		   wait(NULL);
-		   printf("Child terminated");
+
+	if (child_pid == 0)
+	{
+		// Replace current program with calling program
+
+		dup2(fileno(stdout), StdOutErrToParent[1]);
+		dup2(fileno(stderr), StdOutErrToParent[1]);
+		close(StdOutErrToParent[0]);
+		execv(command, &argv[3]);
+	}
+	else
+	{
+		dup2(StdOutErrToParent[0], fileno(stdout));
+		close(StdOutErrToParent[1]);
+		// We're in parent
+		wait(NULL);
 	}
 
 	return 0;
